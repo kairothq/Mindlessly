dc4603b4d141acfa70bfdebd0b0a6266806d05e8
Divy Kairoth <divykairoth@gmail.com>
2025-10-29 04:55:49 +0530

    🎨 UX: Dark mode onboarding + proper attribution
    UI/UX Improvements:
    - Dark mode onboarding page with #1a1a1a background
    - White 'Mindlessly' title with star logo positioned beside it
    - Grey (#2d2d2d) step boxes for better dark mode visibility
    - Updated footer to link to LinkedIn first, then GitHub
    - Enhanced visual hierarchy and contrast
    
    Legal Compliance:
    - Updated LICENSE with proper MIT license attribution
    - Maintained original author credit (Alexander Widua)
    - Added attribution for Mindlessly modifications (Divy Kairoth)
    - Clear distinction between original and modified work
    - Full compliance with MIT license requirements
    
    Better UX + legally compliant open source project!

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@
  */
 
 const footer = document.querySelector('footer')
-footer.innerHTML = `<a href='https://github.com/kairothq/Mindlessly/'>Mindlessly</a>, made by <a href='https://github.com/kairothq/'>Divy Kairoth</a>.
+footer.innerHTML = `<a href='https://github.com/kairothq/Mindlessly/'>Mindlessly</a>, made by <a href='https://www.linkedin.com/in/divy-kairoth/'>Divy Kairoth</a>.
     <br/>
     <ul>
-        <li><a href='https://github.com/kairothq/Mindlessly/'>View Source on GitHub</a></li>
+        <li><a href='https://www.linkedin.com/in/divy-kairoth/'>Connect on LinkedIn</a></li>
         <li>&bull;</li>
-        <li><a href='#'>Privacy Policy</a></li>
+        <li><a href='https://github.com/kairothq/Mindlessly/'>View Source on GitHub</a></li>
     </ul>`
